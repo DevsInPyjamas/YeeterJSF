e7377c53c0b2705eb76e3893399d6ddcf682333f
Jesús Parejo Aliaga <jesusparejo98@gmail.com>
2019-05-21 18:10:30 +0200

    Casi terminado el perfil propio
    estoy a la espara de saber como me van a mandar si es otro usuario para gestionarlo

---
+++ YeeterJSF-war/src/java/yeeter/bean/ProfileBean.java
@@ -0,0 +1,97 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package yeeter.bean;
+
+import javax.annotation.PostConstruct;
+import javax.ejb.EJB;
+import javax.inject.Named;
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Inject;
+import yeeterapp.ejb.UsuarioFacade;
+import yeeterapp.entity.Usuario;
+
+/**
+ *
+ * @author jesus
+ */
+@Named(value = "profileBean")
+@RequestScoped
+public class ProfileBean {
+
+    @EJB
+    private UsuarioFacade usuarioFacade;
+    
+    @Inject
+    private YeeterSessionBean sessionBean;
+    
+    protected boolean isLoggedUser;
+    protected boolean editar = false;
+    protected Usuario usuarioSeleccionado;
+    protected String message;
+
+    /**
+     * Creates a new instance of ProfileBean
+     */
+    public ProfileBean() {
+    }
+
+    public Usuario getUsuarioSeleccionado() {
+        return usuarioSeleccionado;
+    }
+
+    public void setUsuarioSeleccionado(Usuario usuarioSeleccionado) {
+        this.usuarioSeleccionado = usuarioSeleccionado;
+    }
+
+    public boolean isIsLoggedUser() {
+        return isLoggedUser;
+    }
+
+    public void setIsLoggedUser(boolean isLoggedUser) {
+        this.isLoggedUser = isLoggedUser;
+    }
+    
+    public void setIsLoggedUser() {
+        this.isLoggedUser = this.usuarioSeleccionado.equals(this.sessionBean.getLoggedUserObject());
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public boolean isEditar() {
+        return editar;
+    }
+
+    public void setEditar(boolean editar) {
+        this.editar = editar;
+    }
+ 
+    @PostConstruct
+    public void init(){
+        this.usuarioSeleccionado = this.sessionBean.getLoggedUserObject();
+        this.setIsLoggedUser();
+    }
+    
+    public String doModificar(){
+        editar = true;
+        return "profilePanel";
+    }
+    
+    public String doGuardar() {
+        this.usuarioFacade.edit(usuarioSeleccionado);
+        this.init();
+        this.message = "Se ha modificado correctamente el perfil";
+        this.editar = false;
+        System.out.println("yeeey");
+        return "welcomepage";
+    }
+    
+}
