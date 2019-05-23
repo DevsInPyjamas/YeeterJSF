b63719c2af1db0591d97fa70d4546954e79f51ec
Alkesst <alec.augal@gmail.com>
2019-05-23 12:10:53 +0200

    estic fins els ous conyo de cristo de polla

---
+++ YeeterJSF-war/src/java/yeeter/bean/YeeterNotificationsBean.java
@@ -0,0 +1,98 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package yeeter.bean;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import javax.annotation.PostConstruct;
+import javax.ejb.EJB;
+import javax.inject.Named;
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Inject;
+import yeeterapp.ejb.NotificacionesFacade;
+import yeeterapp.ejb.PeticionAmistadFacade;
+import yeeterapp.entity.Notificaciones;
+import yeeterapp.entity.PeticionAmistad;
+// import yeeterapp.entity.PeticionAmistadPK;
+import yeeterapp.entity.Usuario;
+
+/**
+ *
+ * @author alec
+ */
+@Named(value = "yeeterNotificationsBean")
+@RequestScoped
+public class YeeterNotificationsBean {
+
+    @EJB
+    private PeticionAmistadFacade peticionAmistadFacade;
+
+    @EJB
+    private NotificacionesFacade notificacionesFacade;
+        
+    
+    @Inject private YeeterSessionBean yeeterSessionBean;
+
+    private List<Notificaciones> notificationsList;
+    private List<PeticionAmistad> peticionesAmistadList;
+    private Usuario loggedUser;
+    private long notRead;
+    
+    /**
+     * Creates a new instance of YeeterNotificationsBean
+     */
+    public YeeterNotificationsBean() {
+    }
+
+    public List<Notificaciones> getNotificationsList() {
+        return notificationsList;
+    }
+
+    public void setNotificationsList(List<Notificaciones> notificationsList) {
+        this.notificationsList = notificationsList;
+    }
+
+    public List<PeticionAmistad> getPeticionesAmistadList() {
+        return peticionesAmistadList;
+    }
+
+    public void setPeticionesAmistadList(List<PeticionAmistad> peticionesAmistadList) {
+        this.peticionesAmistadList = peticionesAmistadList;
+    }
+    
+    
+    
+    @PostConstruct
+    public void init() {
+        this.loggedUser = yeeterSessionBean.getLoggedUserObject();
+        this.notificationsList = loggedUser.getNotificacionesList();
+        this.peticionesAmistadList = loggedUser.getPeticionAmistadList1();
+        this.notRead = notificationsList.stream().filter(notificacion -> 
+                !notificacion.getNotificacionLeida()).count() ;
+                //+ peticiones.size();
+    }
+    
+    public String doReadAllNotifications() {
+        List<Notificaciones> notificaciones = 
+                    loggedUser.getNotificacionesList().stream().filter(x -> !x.getNotificacionLeida()).collect(Collectors.toList());
+            notificaciones.forEach((not) -> {
+                not.setNotificacionLeida(true);
+                notificacionesFacade.edit(not);
+            });
+        this.init();
+        return null;
+    }
+    
+    public String doReadNotification(int id) {
+        Notificaciones notificacion = notificacionesFacade.find(id);
+        notificacion.setNotificacionLeida(true);
+        notificacionesFacade.edit(notificacion);
+        this.init();
+        return null;
+    }
+    
+    
+}
