6d1658d309c03d1b9d5ce1bcf275573e2aa3bdfc
Jesús Parejo Aliaga <jesusparejo98@gmail.com>
2019-05-23 13:17:34 +0200

    Modificar usuario terminado

--- YeeterJSF-war/src/java/yeeter/bean/ProfileBean.java
+++ YeeterJSF-war/src/java/yeeter/bean/ProfileBean.java
@@ -6,6 +6,8 @@
 package yeeter.bean;
 
 import java.io.Serializable;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import javax.annotation.PostConstruct;
 import javax.ejb.EJB;
 import javax.inject.Named;
@@ -28,10 +30,12 @@ public class ProfileBean implements Serializable {
     @Inject
     private YeeterSessionBean sessionBean;
     
+    private SimpleDateFormat format = new SimpleDateFormat("dd/MM/yyyy");
     protected boolean isLoggedUser;
     protected boolean editar = false;
     protected Usuario usuarioSeleccionado;
     protected String message;
+    protected String fecha;
 
     /**
      * Creates a new instance of ProfileBean
@@ -66,6 +70,14 @@ public class ProfileBean implements Serializable {
     public void setMessage(String message) {
         this.message = message;
     }
+    
+    public String getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(String fecha) {
+        this.fecha = fecha;
+    }
 
     public boolean isEditar() {
         return editar;
@@ -79,6 +91,7 @@ public class ProfileBean implements Serializable {
     public void init(){
         this.usuarioSeleccionado = this.sessionBean.getLoggedUserObject();
         this.setIsLoggedUser();
+        this.fecha = format.format(usuarioSeleccionado.getFechaNacimiento());
     }
     
     public String doModificar(){
@@ -86,12 +99,13 @@ public class ProfileBean implements Serializable {
         return "profilePanel";
     }
     
-    public String doGuardar() {
+    public String doGuardar() throws ParseException {
+        this.usuarioSeleccionado.setFechaNacimiento(format.parse(fecha));
         this.usuarioFacade.edit(usuarioSeleccionado);
         this.init();
         this.message = "Se ha modificado correctamente el perfil";
         this.editar = false;
-        return "welcomepage";
+        return "profilePanel";
     }
     
 }
